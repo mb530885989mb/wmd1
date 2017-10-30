92c1c38eb85ee7b4ba825a189dc156b2ef4a0ae6
mb530885989mb <31686496+mb530885989mb@users.noreply.github.com>
2017-10-30 20:42:30 +0800

    Add files via upload

---
+++ 0.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+void main()
+{
+	cout<<"abcde"<<endl;
+	printf("abcde\n");
+	int a,b,c,d,e;
+	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+	cout<<a<<b<<c<<d<<e<<endl;
+	cin>>a>>b>>c>>d>>e;
+	printf("%d%d%d%d%d\n",a,b,c,d,e);
+}
